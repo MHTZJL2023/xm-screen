3b5cb926936e1f74ef888d474baca8cbf24ac9db
chenr <526963089@qq.com>
2025-03-11 14:40:21 +0800

    feat: update

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,10 @@
  * @FilePath: /vite-project/vite.config.ts
  * @Description:
  */
-import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
