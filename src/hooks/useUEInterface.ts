3c3133ad1d3fdc5fce0061d3c1fe07fe2790a3ff
MHT_ZJL <2275471026@qq.com>
2026-01-04 14:00:54 +0800

    fix: update

--- src/hooks/useUEInterface.ts
+++ src/hooks/useUEInterface.ts
@@ -7,7 +7,7 @@ export const useUEInterface = () => {
   };
 
   const workshop = () => {
-    ueStore.setWorkshop('终检车间');
+    // ueStore.setWorkshop('终检车间');
     window.ue.interface.ClickWorkshop = (res: { data: '总装车间' | '喷涂车间' | '终检车间' | '示范工位' }) => {
       ueStore.setWorkshop(res.data);
     };
